6d1b79742ed6b903d578e85df5653ce4037850bc
Aizhan Dzhumanalieva <msouldze1@gmail.com>
2024-04-30 21:22:26 +0900

    start a new project

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import './Footer.css'
+import TasksFilter from "../TasksFilter"
+
+export default function Footer() {
+    return (
+        <>
+            <footer className="footer">
+                <span className="todo-count">1 items left</span>
+                <TasksFilter />
+                <button className="clear-completed">Clear completed</button>
+            </footer>
+        </>
+    )
+}
---
+++ src/components/TaskList/TaskList.jsx
@@ -0,0 +1,32 @@
+import { formatDistanceToNow } from "date-fns";
+import './TaskList.css';
+import Task from "../Task";
+
+function dateFormatter(date) {
+    const result = formatDistanceToNow(
+        date,
+        {includeSeconds: true}
+    )
+    return result;
+}
+
+function TaskList() {
+    return (
+        <>
+            <ul className="todo-list">
+                <li className="completed">
+                    <Task description={'Completed task'} date={`created ${dateFormatter(new Date(2024, 4, 0, 13, 48, 15))} ago`} />
+                </li>
+                <li className="editing">
+                    <Task description={'Editing task'} date={`created ${dateFormatter(new Date(2024, 4, 0, 10, 15, 0))} ago`} />
+                    <input type="text" className="edit" value="Editing task" />
+                </li>
+                <li>
+                    <Task description={'Active task'} date={`created ${dateFormatter(new Date(2024, 4, 0, 10, 15, 0))} ago`} />
+                </li>
+            </ul>
+        </>
+    )
+}
+
+export default TaskList;
